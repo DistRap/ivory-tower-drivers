f57087e3b3070757344970e4f205c147bc4b948e
Richard Marko <srk@48.io>
2019-07-19 12:34:21 +0200

    add Ivory.Tower.Drivers.Temperature.Types

---
+++ src/Ivory/Tower/Drivers/Temperature/Types.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module Ivory.Tower.Drivers.Temperature.Types where
+
+import Ivory.Language
+import Ivory.Tower.Types.Time
+
+-- Temperature / humidity sample struct types
+-- * temperature units are in degrees Celsius
+-- * humidity is a relative humidity percentage
+
+[ivory|
+ struct sample_th
+  { sample_th_temperature :: Stored IDouble
+  ; sample_th_humidity    :: Stored IDouble
+  ; sample_th_time        :: Stored ITime
+  }
+
+ struct sample_t
+  { sample_t_temperature :: Stored IDouble
+  ; sample_t_time        :: Stored ITime
+  }
+
+ struct sample_h
+  { sample_h_humidity    :: Stored IDouble
+  ; sample_h_time        :: Stored ITime
+  }
+|]
+
+
