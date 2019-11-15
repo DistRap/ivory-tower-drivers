c2b31d7e3d076b105de579bf37decafa66cf1141
Richard Marko <srk@48.io>
2019-11-15 12:11:01 +0100

    ADC/PCF8591: fix warning, drop struct remnants

--- src/Ivory/Tower/Drivers/ADC/PCF8591.hs
+++ src/Ivory/Tower/Drivers/ADC/PCF8591.hs
@@ -7,9 +7,6 @@ module Ivory.Tower.Drivers.ADC.PCF8591 where
 
 import Ivory.Language
 import Ivory.Stdlib
-import Ivory.HW
-import Ivory.HW.BitData
-import Ivory.HW.Reg
 
 import Ivory.Tower
 import Ivory.Tower.HAL.Bus.Interface
@@ -32,9 +29,8 @@ pcf8591Tower :: BackpressureTransmit ('Struct "i2c_transaction_request") ('Struc
             -> Tower e (BackpressureTransmit
                   ('Stored ITime)
                   ADCArray
-                  --('Struct "pcf_sample")
                 , ChanInput ('Stored Uint8))
-pcf8591Tower i2cTransmit@(BackpressureTransmit reqChan resChan) initChan addr = do
+pcf8591Tower i2cTransmit@(BackpressureTransmit _reqChan _resChan) initChan addr = do
   (adcTask, adcReq) <- task "adc"
   tADC <- pcf8591ADCTower adcReq initChan addr
 
@@ -52,7 +48,7 @@ pcf8591ADCTower :: BackpressureTransmit ('Struct "i2c_transaction_request") ('St
                   ('Stored ITime)
                   ADCArray
                 )
-pcf8591ADCTower i2cTransmit@(BackpressureTransmit reqChan resChan) initChan addr = do
+pcf8591ADCTower (BackpressureTransmit reqChan resChan) _initChan addr = do
   readRequest <- channel
   readResponse <- channel
 
@@ -75,33 +71,22 @@ pcf8591ADCTower i2cTransmit@(BackpressureTransmit reqChan resChan) initChan addr
         emit req (constRef r)
 
         _ <- yield
-        r <- local $ istruct
+        r2 <- local $ istruct
               [ tx_addr .= ival addr
               , tx_buf  .= izero
               , tx_len  .= izero
               , rx_len  .= ival 5
               ]
-        emit req (constRef r)
+        emit req (constRef r2)
 
         x <- yield
         rc <- deref (x ~> resultcode)
         when (rc ==? 0) $ do
-          -- skip first byte with previous reading
-          c0 <- deref ((x ~> rx_buf) ! 1) -- channel 0
-          c1 <- deref ((x ~> rx_buf) ! 2)
-          c2 <- deref ((x ~> rx_buf) ! 3)
-          c3 <- deref ((x ~> rx_buf) ! 4)
-
           arrayMap $ \i -> do
+            -- skip first byte with previous reading
             val <- deref ((x ~> rx_buf) ! (toIx $ fromIx i + 1))
             store (adcLast ! i) val
 
-          s <- local $ istruct [
-              pcf_channel0 .= ival c0
-            , pcf_channel1 .= ival c1
-            , pcf_channel2 .= ival c2
-            , pcf_channel3 .= ival c3
-            ]
           emit e (constRef adcLast)
 
   return $ BackpressureTransmit
@@ -112,7 +97,7 @@ pcf8591DACTower :: BackpressureTransmit ('Struct "i2c_transaction_request") ('St
             -> ChanOutput ('Stored ITime)
             -> I2CDeviceAddr
             -> Tower e (ChanInput ('Stored Uint8))
-pcf8591DACTower i2cTransmit@(BackpressureTransmit reqChan resChan) initChan addr = do
+pcf8591DACTower (BackpressureTransmit reqChan _resChan) _initChan addr = do
   writeChan <- channel
 
   monitor "pcf8591" $ do
