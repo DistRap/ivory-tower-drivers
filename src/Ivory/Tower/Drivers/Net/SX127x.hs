415fa9cdb92fb1951939839ea3539deaae1ad1e5
Richard Marko <srk@48.io>
2020-10-12 14:51:02 +0200

    net/sx127x: reuse spi request structs, drop IQ inversion setting

--- src/Ivory/Tower/Drivers/Net/SX127x.hs
+++ src/Ivory/Tower/Drivers/Net/SX127x.hs
@@ -10,6 +10,7 @@
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeOperators #-}
 
 
 module Ivory.Tower.Drivers.Net.SX127x where
@@ -64,7 +65,7 @@ radioDriverTypes = package "radioDriverTypes" $ do
 -- todo
 -- ttn sync word 0x34
 
-sxTower (BackpressureTransmit req res) rdy spiDev name pin receiver = do
+sxTower (BackpressureTransmit req res) rdy spiDev name pin = do
   towerModule radioDriverTypes
   towerDepends radioDriverTypes
 
@@ -78,7 +79,9 @@ sxTower (BackpressureTransmit req res) rdy spiDev name pin receiver = do
   radioRxDone <- channel
 
   monitor "rfm" $ do
-    --dbg <- state (named "dbg")
+    dbgIRQFlags <- state (named "dbgIRQFlags")
+    dbgModemStatus <- state (named "dbgModemStatus")
+    busy <- state (named "busy")
 
     freq <- state (named "freq")
 
@@ -103,6 +106,8 @@ sxTower (BackpressureTransmit req res) rdy spiDev name pin receiver = do
     resetDevice <- state (named "resetDevice")
     resetDone   <- state (named "resetDone")
     resLow      <- state (named "resLow")
+
+    spiReq <- stateInit (named "spiReq") (istruct [ tx_device .= ival spiDev ])
 
     handler systemInit (named "pinSet") $ do
       callback $ const $ do
@@ -131,8 +136,8 @@ sxTower (BackpressureTransmit req res) rdy spiDev name pin receiver = do
               store resetDevice false
               store resetDone true
 
-              r <- sxRead spiDev (sxModemStatus sx127x)
-              emit reqE r
+              sxRead (sxModemStatus sx127x) spiReq
+              emit reqE (constRef spiReq)
             )
 
     handler (snd radioTx) (named "transmitPeriod") $ do
@@ -156,9 +161,10 @@ sxTower (BackpressureTransmit req res) rdy spiDev name pin receiver = do
       reqE <- emitter req 1
       callback $ const $ do
         ok <- deref isReady
-        when ok $ do
-          r <- sxRead spiDev (sxModemStatus sx127x)
-          emit reqE r
+        busy' <- deref busy
+        when (ok .&& iNot busy') $ do
+          sxRead (sxModemStatus sx127x) spiReq
+          emit reqE (constRef spiReq)
 
     coroutineHandler rdy res "sxCoro" $ do
       reqE <- emitter req 1
@@ -166,10 +172,14 @@ sxTower (BackpressureTransmit req res) rdy spiDev name pin receiver = do
       radioRxDoneE <- emitter (fst radioRxDone) 1
       radioTxDoneE <- emitter (fst radioTxDone) 1
       return $ CoroutineBody $ \yield -> do
-        let rpc x = do
-              r <- x
-              emit reqE r
-              yield
+
+        let rpc createReq = do
+              createReq spiReq
+              store busy true
+              emit reqE (constRef spiReq)
+              x <- yield
+              store busy false
+              return x
 
             read x = do
               res <- rpc x
@@ -183,10 +193,10 @@ sxTower (BackpressureTransmit req res) rdy spiDev name pin receiver = do
                 store (arr ! 1) (bitCast $ frf `iShiftR` 8)
                 store (arr ! 2) (bitCast $ frf)
 
-                write $ sxWriteArray spiDev (sxFrfMsb sx127x) (constRef arr) 3
+                write $ sxWriteArray (sxFrfMsb sx127x) (constRef arr) 3
 
             getFrequency = do
-                dat <- rpc $ sxReadMany spiDev (sxFrfMsb sx127x) 3
+                dat <- rpc $ sxReadN (sxFrfMsb sx127x) 3
                 hi  <- fmap safeCast $ deref (dat ~> rx_buf ! 1)
                 mid <- fmap safeCast $ deref (dat ~> rx_buf ! 2)
                 low <- fmap safeCast $ deref (dat ~> rx_buf ! 3)
@@ -194,17 +204,17 @@ sxTower (BackpressureTransmit req res) rdy spiDev name pin receiver = do
                 store freq $ frf * 32_000_000 `iShiftR` 19
 
             switchMode opmode = do
-                prevRes <- rpc $ sxRead spiDev (sxMode sx127x)
-                prev <- deref (prevRes ~> rx_buf ! 1)
+                prev <- read $ sxRead (sxMode sx127x)
                 mode <- assign $ repToBits $ withBits prev $ do
                   setField op_mode_mode opmode
 
-                write $ sxWrite spiDev (sxMode sx127x) mode
+                write $ sxWrite (sxMode sx127x) mode
                 comment "Make sure we've switched mode"
                 forever $ do
-                  x <- noBreak $ rpc $ sxRead spiDev (sxMode sx127x)
-                  vers <- fmap fromRep $ deref (x ~> rx_buf ! 1)
-                  when (vers #. op_mode_mode ==? opmode) breakOut
+                  x <- noBreak $ read $ sxRead (sxMode sx127x)
+                  let mode' = fromRep x
+                  when (mode' #. op_mode_mode ==? opmode) breakOut
+                  noBreak $ write $ sxWrite (sxMode sx127x) mode
 
             intToBits :: Int -> Bits 8
             intToBits x = repToBits $ fromIntegral x
@@ -214,7 +224,7 @@ sxTower (BackpressureTransmit req res) rdy spiDev name pin receiver = do
         _ <- yield
 
         forever $ do
-          vers <- noBreak $ read $ sxRead spiDev (sxVersion sx127x)
+          vers <- noBreak $ read $ sxRead (sxVersion sx127x)
           when (vers ==? 0x12) breakOut
 
         comment "Put radio to sleep"
@@ -226,7 +236,7 @@ sxTower (BackpressureTransmit req res) rdy spiDev name pin receiver = do
           -- for HF
           clearBit op_mode_low_frequency_mode_on
 
-        write $ sxWrite spiDev (sxMode sx127x) lora
+        write $ sxWrite (sxMode sx127x) lora
 
         --setFrequency 868_000_000
         setFrequency 868_100_000
@@ -247,16 +257,16 @@ sxTower (BackpressureTransmit req res) rdy spiDev name pin receiver = do
         --modem3 <- assign $ repToBits $ withBits 0 $ do
         --  setBit modem_config3_agc_auto_on
 
-        write $ sxWrite spiDev (sxModemConfig1 sx127x) modem1
-        write $ sxWrite spiDev (sxModemConfig2 sx127x) modem2
+        write $ sxWrite (sxModemConfig1 sx127x) modem1
+        write $ sxWrite (sxModemConfig2 sx127x) modem2
         --write $ sxWrite spiDev (sxModemConfig3 sx127x) modem3
 
         comment "Configure FIFOs"
-        write $ sxWrite spiDev (sxFIFORxBaseAddr sx127x) (repToBits $ fromIntegral (0x0 :: Int))
-        write $ sxWrite spiDev (sxFIFOTxBaseAddr sx127x) (repToBits $ fromIntegral (0x0 :: Int))
+        write $ sxWrite (sxFIFORxBaseAddr sx127x) (repToBits $ fromIntegral (0x0 :: Int))
+        write $ sxWrite (sxFIFOTxBaseAddr sx127x) (repToBits $ fromIntegral (0x0 :: Int))
 
         comment "Max payload length"
-        write $ sxWrite spiDev (sxMaxPayloadLength sx127x)
+        write $ sxWrite (sxMaxPayloadLength sx127x)
                  (repToBits $ fromIntegral (0x80 :: Int))
 
         comment "Power"
@@ -266,17 +276,13 @@ sxTower (BackpressureTransmit req res) rdy spiDev name pin receiver = do
           -- when PA_BOOST
           -- Pout = 2dB + output_power
           setField pa_config_output_power (fromRep 0x0)
-        write $ sxWrite spiDev (sxPAConfig sx127x) power
+        write $ sxWrite (sxPAConfig sx127x) power
 
-        -- XXX
-        unless receiver $ do
-          write $ sxWrite spiDev (sxDIOMapping1 sx127x) (repToBits $ fromIntegral (0x40 :: Int))
+        -- DIO0 ISR on rxdone and txdone
+        write $ sxWrite (sxDIOMapping1 sx127x) (repToBits $ fromIntegral (0b11 :: Int))
 
         comment "Stand-by"
         switchMode mode_standby
-
-        -- XXX - WAN
-        write $ sxWrite spiDev (sxSyncWord sx127x) (intToBits 0x34)
 
         store isReady true
         emitV radioRdyE true
@@ -285,12 +291,13 @@ sxTower (BackpressureTransmit req res) rdy spiDev name pin receiver = do
           -- we get status from polling handler above
           -- XXX: switch to exti
           mStatus <- yield
+          refCopy dbgModemStatus mStatus
 
           --x <- rpc $ sxRead spiDev (sxMode sx127x)
           --refCopy dbg x
 
-          i <- rpc $ sxRead spiDev (sxIRQFlags sx127x)
-          --refCopy dbgisr i
+          i <- rpc $ sxRead (sxIRQFlags sx127x)
+          refCopy dbgIRQFlags i
 
           is <- deref (i ~> rx_buf ! 1)
           isrs <- assign $ fromRep is
@@ -304,24 +311,23 @@ sxTower (BackpressureTransmit req res) rdy spiDev name pin receiver = do
 
             , bitToBool (isrs #. irq_flags_rx_done) ==> do
                 rxCount += 1
-                len <- read $ sxRead spiDev (sxRxLength sx127x)
+                len <- read $ sxRead (sxRxLength sx127x)
                 store (rxRes ~> radio_rx_len) (toIx len)
 
                 -- RSSI[dBm] = -164 + Rssi (using LF output port, SNR >= 0)
                 -- RSSI[dBm] = -157 + Rssi (using HF output port, SNR >= 0)
-                i <- read $ sxRead spiDev (sxPacketRSSI sx127x)
-                s <- read $ sxRead spiDev (sxPacketSNR sx127x)
+                i <- read $ sxRead (sxPacketRSSI sx127x)
+                s <- read $ sxRead (sxPacketSNR sx127x)
                 store (rxRes ~> radio_rx_rssi) (safeCast (i - 157))
-                store (rxRes ~> radio_rx_snr) (safeCast (twosComplementCast s `iDiv` 4))
+                store (rxRes ~> radio_rx_snr) (safeCast (twosComplementCast s / 4))
 
                 -- set FIFO to RxCurrentAddr
-                rxAddr <- read $ sxRead spiDev (sxFIFORxCurrentAddr sx127x)
+                rxAddr <- read $ sxRead (sxFIFORxCurrentAddr sx127x)
 
-                write $ sxWrite spiDev (sxFIFOAddr sx127x) (fromRep rxAddr)
-                dat <- rpc $ sxReadMany spiDev (sxFIFO sx127x) len
-
+                write $ sxWrite (sxFIFOAddr sx127x) (fromRep rxAddr)
                 -- Read all received bytes
-                w <-  sxReadMany spiDev (sxFIFO sx127x) len
+                dat <- rpc $ sxReadN (sxFIFO sx127x) len
+
                 -- we need to skip rx_buf ! 0
                 arrayMap $ \i -> do
                   cond_ [
@@ -338,24 +344,21 @@ sxTower (BackpressureTransmit req res) rdy spiDev name pin receiver = do
 
           comment "Clear IRQ flags"
           when (is /=? 0) $ do
-            write $ sxWrite spiDev (sxIRQFlags sx127x) (repToBits is)
+            write $ sxWrite (sxIRQFlags sx127x) (repToBits is)
 
           tx <- deref isTX
           when tx $ do
 
             switchMode mode_standby
-            inv <- assign $ repToBits $ withBits 0x27 $ do
-              clearBit invert_iq_inverted
-            write $ sxWrite spiDev (sxInvertIQ sx127x) inv
 
-            write $ sxWrite spiDev (sxFIFOAddr sx127x) (intToBits 0x0)
+            write $ sxWrite (sxFIFOAddr sx127x) (intToBits 0x0)
 
             len <- fmap ixToU8 $ deref (txReq ~> radio_tx_len)
 
-            write $ sxWrite spiDev (sxPayloadLength sx127x)
+            write $ sxWrite (sxPayloadLength sx127x)
               (repToBits $ len)
 
-            write $ sxWriteArray spiDev (sxFIFO sx127x)
+            write $ sxWriteArray (sxFIFO sx127x)
               (constRef $ txReq ~> radio_tx_buf) len
 
             switchMode mode_tx
@@ -373,13 +376,7 @@ sxTower (BackpressureTransmit req res) rdy spiDev name pin receiver = do
               setField lna_gain_lna_gain lna_gain_g1
               setField lna_gain_lna_boost_hf (fromRep 0b11)
 
-            write $ sxWrite spiDev (sxLNAGain sx127x) lna
-
-            -- 0x27 but why
-            inv <- assign $ repToBits $ withBits 0x27 $ do
-              setBit invert_iq_inverted
-
-            write $ sxWrite spiDev (sxInvertIQ sx127x) inv
+            write $ sxWrite (sxLNAGain sx127x) lna
 
             modeCont <- deref (rxReq ~> radio_listen_continuous)
             switchMode (modeCont ? (mode_rxcontinuous, mode_rxsingle))
@@ -396,74 +393,49 @@ sxTower (BackpressureTransmit req res) rdy spiDev name pin receiver = do
   where
     named x = intercalate "_" [ "sx127x", name, x ]
 
-sxRead  :: (GetAlloc eff ~ 'Scope s)
-        => SPIDeviceHandle
-        -> BitDataReg a
-        -> Ivory eff (ConstRef ('Stack s) ('Struct "spi_transaction_request"))
-sxRead dev bdr = fmap constRef $ local $ istruct
-  [ tx_device .= ival dev
-  , tx_buf .= iarray ( map ival [fromIntegral $ regAddr bdr, 0] )
-  , tx_len .= ival 2 ]
+sxReadN
+  :: BitDataReg a
+  -> Uint8
+  -> Ref s ('Struct "spi_transaction_request")
+  -> Ivory eff ()
+sxReadN bdr len r = do
+  store (r ~> tx_buf ! 0) (fromIntegral $ regAddr bdr)
+  store (r ~> tx_buf ! 1) 0
+  store (r ~> tx_len) (toIx $ len + 1)
 
-sxWrite  :: (GetAlloc eff ~ 'Scope s)
-         => SPIDeviceHandle
-         -> BitDataReg a
-         -> Bits 8
-         -> Ivory eff (ConstRef ('Stack s) ('Struct "spi_transaction_request"))
-sxWrite dev bdr dat = fmap constRef $ local $ istruct
-  [ tx_device .= ival dev
-  , tx_buf .= iarray (map ival
-    [ 0x80 .| (fromIntegral $ regAddr bdr)
-    , toRep dat]
-  )
-  , tx_len .= ival 2 ]
+sxRead
+  :: BitDataReg a
+  -> Ref s ('Struct "spi_transaction_request")
+  -> Ivory eff ()
+sxRead bdr = sxReadN bdr 1
 
-sxWriteMany  :: (GetAlloc eff ~ 'Scope s)
-         => SPIDeviceHandle
-         -> BitDataReg a
-         -> [Int]
-         -> Ivory eff (ConstRef ('Stack s) ('Struct "spi_transaction_request"))
-sxWriteMany dev bdr dat = fmap constRef $ local $ istruct
-  [ tx_device .= ival dev
-  , tx_buf .= iarray (map ival
-    [ 0x80 .| (fromIntegral $ regAddr bdr)]
-    ++ (map (ival . fromIntegral) dat)
-  )
-  , tx_len .= ival (fromIntegral $ length dat + 1) ]
+sxWrite
+  :: BitDataReg a
+  -> Bits 8
+  -> Ref s ('Struct "spi_transaction_request")
+  -> Ivory eff ()
+sxWrite bdr dat r = do
+  store (r ~> tx_buf ! 0) (0x80 .| (fromIntegral $ regAddr bdr))
+  store (r ~> tx_buf ! 1) (toRep dat)
+  store (r ~> tx_len) 2
 
-sxWriteArray  :: (GetAlloc eff ~ 'Scope s)
-         => SPIDeviceHandle
-         -> BitDataReg a
-         -> ConstRef s2 SXArray
-         -> Uint8
-         -> Ivory eff (ConstRef ('Stack s) ('Struct "spi_transaction_request"))
-sxWriteArray dev bdr dat len = do
-  header <- local $ istruct
-    [ tx_device .= ival dev
-    , tx_buf .= iarray [ ival $ 0x80 .| (fromIntegral $ regAddr bdr) ]
-    , tx_len .= ival (toIx $ len + 1) ]
+sxWriteArray
+  :: BitDataReg a
+  -> ConstRef s2 SXArray
+  -> Uint8
+  -> Ref s ('Struct "spi_transaction_request")
+  -> Ivory eff ()
+sxWriteArray bdr dat len r = do
+  store (r ~> tx_buf ! 0) (0x80 .| (fromIntegral $ regAddr bdr))
+  store (r ~> tx_len) (toIx $ len + 1)
 
   arrayMap $ \i -> do
     cond_ [
         fromIx i >=? safeCast len ==> return ()
       , true      ==> do
           x <- deref (dat ! i)
-          store (header ~> tx_buf ! (toIx (fromIx i) + 1)) x
+          store (r ~> tx_buf ! (toIx (fromIx i) + 1)) x
       ]
-
-  return $ constRef $ header
-
-sxReadMany  :: (GetAlloc eff ~ 'Scope s)
-         => SPIDeviceHandle
-         -> BitDataReg a
-         -> Uint8
-         -> Ivory eff (ConstRef ('Stack s) ('Struct "spi_transaction_request"))
-sxReadMany dev bdr len = fmap constRef $ local $ istruct
-  [ tx_device .= ival dev
-  , tx_buf .= iarray (map ival
-    [ fromIntegral $ regAddr bdr]
-  )
-  , tx_len .= ival (toIx $ len + 1) ]
 
 regAddr :: BitDataReg d -> Integer
 regAddr r = case bdr_reg r of Reg a -> a
